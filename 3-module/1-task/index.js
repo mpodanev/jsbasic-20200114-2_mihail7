86b087cdc5d7592ba5fc00a460c91d3903972fb2
mpodanev <mpodanev@gmail.com>
2020-01-26 17:45:16 +0200

    implement showSalary function

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,10 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  return data.filter((el) => el.age <= age).reduce((acc, it) => {
+    if (acc === '') {
+      return acc += it.name + ', ' + it.balance;
+    }
+    return acc += '\n' + it.name + ', ' + it.balance;
+  }, '');
 }
