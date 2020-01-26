bd8cb097c8bba5f8171d26f1fd252c46b151349f
mpodanev <mpodanev@gmail.com>
2020-01-26 22:48:53 +0200

    implement getMinMax function

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  const arr = str.split(' ');
+  let arr2 = [3];
+  for (const it of arr) {
+    arr2 = arr2.concat(it.split(','));
+  }
+  const arr3 = arr2.map(it => parseFloat(it)).filter(it => !isNaN(it)).sort((a, b) => a - b);
+  const obj = {};
+  obj.min = arr3[0];
+  obj.max = arr3[arr3.length - 1];
+  return obj;
 }
