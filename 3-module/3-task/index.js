274b97d601c81e0c70682b397a78ff60714914fc
mpodanev <mpodanev@gmail.com>
2020-01-29 07:48:49 +0200

    implement camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,26 @@
  * @returns {string}
  */
 function camelize(str) {
+  let finalString = '';
+  let flag = false;
+  const arr = str.split('');
+
+  for (const el of arr) {
+    if (el === '-') {
+      flag = true;
+      continue;
+    }
+
+    if (flag === true) {
+      finalString += el.toUpperCase();
+      flag = false;
+      continue;
+    }
+
+    if (el !== '-') {
+      finalString += el;
+    }
+  }
+
+  return finalString;
 }
