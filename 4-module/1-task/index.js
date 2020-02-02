6c3bfc6ff5ccf85fd6253ca9513434523a557bc9
mpodanev <mpodanev@gmail.com>
2020-02-02 10:08:47 +0200

    implement makeFriendsList and makeDiagonalRed functions

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,11 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const list = document.createElement('ul');
+  let itemsStr = '';
+  for (const el of friends) {
+    itemsStr += `<li>${el.firstName} ${el.lastName}</li>`;
+  }
+  list.innerHTML = itemsStr;
+  return list;
 }
