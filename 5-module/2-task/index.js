683e881c351bad239ce741ab62cd2e011d476510
mpodanev <mpodanev@gmail.com>
2020-02-10 07:31:53 +0200

    implement SortableTable function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,6 +18,27 @@ function SortableTable(items) {
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  this.head = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>';
+  this.el.innerHTML = this.head;
+  this.tbody = document.createElement('tbody');
+  this.items = items;
+
+  this.create = function(items) {
+
+    for (const it of items) {
+      const tr = document.createElement('tr');
+      tr.insertAdjacentHTML('beforeend', `<td>${it.name}</td>`);
+      tr.insertAdjacentHTML('beforeend', `<td>${it.age}</td>`);
+      tr.insertAdjacentHTML('beforeend', `<td>${it.salary}</td>`);
+      tr.insertAdjacentHTML('beforeend', `<td>${it.city}</td>`);
+      this.tbody.insertAdjacentElement('beforeend', tr);
+    }
+
+    this.el.insertAdjacentElement('beforeend', this.tbody);
+
+  };
+
+  this.create(this.items);
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +46,37 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    let sortableItem = '';
+
+    switch (column) {
+      case (0):
+        sortableItem = 'name';
+        break;
+      case (2):
+        sortableItem = 'salary';
+        break;
+    }
+
+    const sort = (a, b) => {
+      if (a[sortableItem] > b[sortableItem]) {return 1;}
+      if (a[sortableItem] === b[sortableItem]) {return 0;}
+      if (a[sortableItem] < b[sortableItem]) {return -1;}
+    };
+    const reverse = (a, b) => {
+      if (a[sortableItem] > b[sortableItem]) {return 1;}
+      if (a[sortableItem] === b[sortableItem]) {return 0;}
+      if (a[sortableItem] < b[sortableItem]) {return -1;}
+    };
+
+    if (!desc) {
+      this.items.sort(sort);
+    } else {
+      this.items.reverse(reverse);
+    }
+
+
+    this.el.querySelector('tbody').innerHTML = '';
+    this.create(this.items);
+  };
 }
